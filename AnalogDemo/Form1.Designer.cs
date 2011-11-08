9a64d4cc5dfc36a2647f70b0da42aa56bdb5478e
unknown <dentellaluca@3LITS063.ingdirect.idi.it>
2011-11-08 21:17:43 +0100

    first commit

---
+++ AnalogDemo/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace AnalogDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.cbSerialPort = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbSendData = new System.Windows.Forms.CheckBox();
+            this.tbAPIKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFeedId = new System.Windows.Forms.TextBox();
+            this.tbDatastreamId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btConnect);
+            this.groupBox1.Controls.Add(this.cbSerialPort);
+            this.groupBox1.Location = new System.Drawing.Point(13, 275);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(135, 75);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial port";
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(6, 46);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(121, 23);
+            this.btConnect.TabIndex = 1;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // cbSerialPort
+            // 
+            this.cbSerialPort.FormattingEnabled = true;
+            this.cbSerialPort.Location = new System.Drawing.Point(6, 19);
+            this.cbSerialPort.Name = "cbSerialPort";
+            this.cbSerialPort.Size = new System.Drawing.Size(121, 21);
+            this.cbSerialPort.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbDatastreamId);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.tbFeedId);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.cbSendData);
+            this.groupBox2.Controls.Add(this.tbAPIKey);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(154, 275);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(468, 74);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pachube";
+            // 
+            // cbSendData
+            // 
+            this.cbSendData.AutoSize = true;
+            this.cbSendData.Location = new System.Drawing.Point(387, 20);
+            this.cbSendData.Name = "cbSendData";
+            this.cbSendData.Size = new System.Drawing.Size(75, 17);
+            this.cbSendData.TabIndex = 2;
+            this.cbSendData.Text = "Send data";
+            this.cbSendData.UseVisualStyleBackColor = true;
+            // 
+            // tbAPIKey
+            // 
+            this.tbAPIKey.Location = new System.Drawing.Point(57, 20);
+            this.tbAPIKey.Name = "tbAPIKey";
+            this.tbAPIKey.Size = new System.Drawing.Size(324, 20);
+            this.tbAPIKey.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "API Key";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(13, 13);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(603, 256);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Feed ID";
+            // 
+            // tbFeedId
+            // 
+            this.tbFeedId.Location = new System.Drawing.Point(57, 48);
+            this.tbFeedId.Name = "tbFeedId";
+            this.tbFeedId.Size = new System.Drawing.Size(156, 20);
+            this.tbFeedId.TabIndex = 4;
+            // 
+            // tbDatastreamId
+            // 
+            this.tbDatastreamId.Location = new System.Drawing.Point(300, 48);
+            this.tbDatastreamId.Name = "tbDatastreamId";
+            this.tbDatastreamId.Size = new System.Drawing.Size(162, 20);
+            this.tbDatastreamId.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Datastream ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 362);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AnalogDemo 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.ComboBox cbSerialPort;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cbSendData;
+        private System.Windows.Forms.TextBox tbAPIKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox tbDatastreamId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbFeedId;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AnalogDemo/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Net;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace AnalogDemo
+{
+    public partial class Form1 : Form
+    {
+
+        private const int CHART_SECONDS = 300;
+        
+        private SerialPort serialPort;
+        private Boolean connected;
+        private WebClient webClient;
+        private Series mySerie;
+
+        public delegate void AddDataDelegate();
+        public AddDataDelegate addDataDel;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            connected = false;
+            webClient = new WebClient();
+            addDataDel += new AddDataDelegate(AddData);
+
+            DateTime minValue = DateTime.Now;
+            DateTime maxValue = minValue.AddSeconds(CHART_SECONDS);
+            chart1.ChartAreas[0].AxisX.Minimum = minValue.ToOADate();
+            chart1.ChartAreas[0].AxisX.Maximum = maxValue.ToOADate();
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "T";
+            chart1.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Minutes;
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.DarkGray;
+            chart1.ChartAreas[0].AxisX.MinorGrid.IntervalType = DateTimeIntervalType.Seconds;
+            chart1.ChartAreas[0].AxisX.MinorGrid.Interval = 10;
+            chart1.ChartAreas[0].AxisX.MinorGrid.LineColor = Color.LightGray;
+            chart1.ChartAreas[0].AxisX.MinorGrid.Enabled = true;
+
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 1023;
+            chart1.ChartAreas[0].AxisY.IntervalType = DateTimeIntervalType.Number;
+            chart1.ChartAreas[0].AxisY.Interval = 200;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.DarkGray;
+            chart1.ChartAreas[0].AxisY.MinorGrid.IntervalType = DateTimeIntervalType.Number;
+            chart1.ChartAreas[0].AxisY.MinorGrid.Interval = 50;
+            chart1.ChartAreas[0].AxisY.MinorGrid.LineColor = Color.LightGray;
+            chart1.ChartAreas[0].AxisY.MinorGrid.Enabled = true;
+
+            chart1.Series.Clear();
+            mySerie = new Series("AnalogInput");
+            mySerie.ChartType = SeriesChartType.FastLine;
+            mySerie.BorderWidth = 1;
+            mySerie.Color = Color.Red;
+            mySerie.XValueType = ChartValueType.DateTime;
+            chart1.Series.Add(mySerie);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {         
+            string[] serialPortNames = SerialPort.GetPortNames();
+            foreach(string serialPortName in serialPortNames)
+                cbSerialPort.Items.Add(serialPortName);
+            if (serialPortNames.Length > 0)
+                cbSerialPort.SelectedIndex = 0;
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            if (!connected)
+            {
+                string serialPortName = cbSerialPort.SelectedItem.ToString();
+                serialPort = new SerialPort(serialPortName, 9600);
+                serialPort.Open();
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(serialPort_DataReceived);
+                btConnect.Text = "Disconnect";
+                cbSerialPort.Enabled = false;
+                connected = true;
+            }
+            else
+            {
+                serialPort.Close();
+                btConnect.Text = "Connect";
+                cbSerialPort.Enabled = true;
+                connected = false;
+            }
+        }
+
+        void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            chart1.Invoke(addDataDel);
+        }
+
+        public void AddData()
+        {
+            DateTime timeStamp = DateTime.Now;
+            string stringValue = serialPort.ReadLine();
+            chart1.Series[0].Points.AddXY(timeStamp, Double.Parse(stringValue));
+
+            double removeBefore = timeStamp.AddSeconds(-CHART_SECONDS).ToOADate();
+            while (mySerie.Points[0].XValue < removeBefore)
+            {
+                mySerie.Points.RemoveAt(0);
+            }
+            chart1.ChartAreas[0].AxisX.Minimum = mySerie.Points[0].XValue;
+            chart1.ChartAreas[0].AxisX.Maximum = DateTime.FromOADate(mySerie.Points[0].XValue).AddSeconds(CHART_SECONDS).ToOADate();
+            chart1.Invalidate();
+
+            if (cbSendData.Checked)
+            {
+                if (tbAPIKey.Text.Trim().Equals("") || tbFeedId.Text.Trim().Equals("") ||
+                    tbDatastreamId.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("Missing Pachube informations", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    cbSendData.Checked = false;
+                }
+
+                string updatePath = "http://api.pachube.com/v2/feeds/" + tbFeedId.Text + "/datastreams/" + tbDatastreamId.Text + ".csv";
+                webClient.Headers.Set("X-PachubeApiKey", tbAPIKey.Text);
+                try
+                {
+                    webClient.UploadString(updatePath, "PUT", stringValue);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Unable to send data to Pachube:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    cbSendData.Checked = false;
+
+                }
+            }
+        }
+    }
+}
